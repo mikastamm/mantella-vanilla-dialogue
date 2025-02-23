28e2b04102a93dfe8e5545e277d305336db43b69
Mika Stamm <mika.stamm1@gmail.com>
2025-02-23 14:02:49 +0100

    Prevent duplicate events in some cases

--- MantellaPapyrusInterface.h
+++ MantellaPapyrusInterface.h
@@ -1,64 +1,73 @@
 #pragma once
 
 namespace MantellaPapyrusInterface {
+    static inline std::string PreviousSentEvent = "";
 
-
- void AddMantellaEvent(std::string msg) {
-    auto targetFunction = "AddMantellaEvent";
-    auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
-    auto mantellaQuest = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESQuest>(0x03D41A, "Mantella.esp");
-    auto questHandle = vm->GetObjectHandlePolicy()->GetHandleForObject(mantellaQuest->GetFormType(), mantellaQuest);
-    RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor> callback;
-    RE::BSTSmartPointer<RE::BSScript::Object> script = nullptr;
-    if (vm->FindBoundObject(questHandle, "MantellaInterface", script)) {
-        auto args = RE::MakeFunctionArguments(std::move(msg));
-        vm->DispatchMethodCall1(script, targetFunction, args, callback);
-    }
-}
-
-RE::VMHandle GetMantellaRepositoryHandle() {
-    auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
-    auto mantellaQuest = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESQuest>(0xD62, "Mantella.esp");
-    auto questHandle = vm->GetObjectHandlePolicy()->GetHandleForObject(mantellaQuest->GetFormType(), mantellaQuest);
-    return questHandle;
-}
-
- bool GetMantellaMcmSetting(std::string propertyName, RE::BSScript::Variable& a_getVal) {
-    auto questHandle = GetMantellaRepositoryHandle();
-    auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
-    RE::BSTSmartPointer<RE::BSScript::Object> script = nullptr;
-    if (vm->FindBoundObject(questHandle, "MantellaRepository", script)) {
-        auto propertyPointer = script->GetProperty(propertyName);
-        if (!propertyPointer) {
-            logger::error("Failed to get Mantella setting {}", propertyName);
-            return false;
+    void AddMantellaEvent(std::string msg, bool deduplicate = true) {
+        if (deduplicate && msg == PreviousSentEvent) {
+            logger::debug("Skipping duplicate event: {}", msg);
+            return;
         }
-        a_getVal = *propertyPointer;
+
+        auto targetFunction = "AddMantellaEvent";
+        auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+        auto mantellaQuest = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESQuest>(0x03D41A, "Mantella.esp");
+        auto questHandle = vm->GetObjectHandlePolicy()->GetHandleForObject(mantellaQuest->GetFormType(), mantellaQuest);
+        RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor> callback;
+        RE::BSTSmartPointer<RE::BSScript::Object> script = nullptr;
+        if (vm->FindBoundObject(questHandle, "MantellaInterface", script)) {
+            auto args = RE::MakeFunctionArguments(std::move(msg));
+            vm->DispatchMethodCall1(script, targetFunction, args, callback);
+            PreviousSentEvent = msg;
+        } else {
+            logger::error("!!! Failed to find MantellaInterface script");
+        }
+    }
+
+    RE::VMHandle GetMantellaRepositoryHandle() {
+        auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+        auto mantellaQuest = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESQuest>(0xD62, "Mantella.esp");
+        auto questHandle = vm->GetObjectHandlePolicy()->GetHandleForObject(mantellaQuest->GetFormType(), mantellaQuest);
+        return questHandle;
+    }
+
+    bool GetMantellaMcmSetting(std::string propertyName, RE::BSScript::Variable& a_getVal) {
+        auto questHandle = GetMantellaRepositoryHandle();
+        auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+        RE::BSTSmartPointer<RE::BSScript::Object> script = nullptr;
+        if (vm->FindBoundObject(questHandle, "MantellaRepository", script)) {
+            auto propertyPointer = script->GetProperty(propertyName);
+            if (!propertyPointer) {
+                logger::error("!!! Failed to get Mantella setting {}", propertyName);
+                return false;
+            }
+            a_getVal = *propertyPointer;
+            return true;
+        }
+        logger::error("!!! Failed to find MantellaRepository script");
+        return false;
+    }
+
+    bool GetMantellaEnableVanillaDialogueAwareness() {
+        RE::BSScript::Variable property;
+        auto result = GetMantellaMcmSetting("enableVanillaDialogueAwareness", property);
+
+        if (result && property.IsBool())
+            return property.GetBool();
+        else
+            logger::error("!!! Failed to get Mantella setting enableVanillaDialogueAwareness");
         return true;
     }
-    return false;
-}
 
- bool GetMantellaEnableVanillaDialogueAwareness() {
-    RE::BSScript::Variable property;
-    auto result = GetMantellaMcmSetting("enableVanillaDialogueAwareness", property);
-   
-    if (result && property.IsBool()) 
-        return property.GetBool();
-    else
-        logger::error("Failed to get Mantella setting enableVanillaDialogueAwareness");
-    return true;
-}
-
-/// <returns>The port the mantella server is reachable under (From the Mcm)</returns>
- int GetMantellaServerPort() { 
-    RE::BSScript::Variable property;
-    auto result = GetMantellaMcmSetting("HttpPort", property);
-    if (result && property.IsInt()) {
-       int port = property.GetSInt();
-        return port;
+    /// <returns>The port the mantella server is reachable under (From the Mcm)</returns>
+    int GetMantellaServerPort() {
+        RE::BSScript::Variable property;
+        auto result = GetMantellaMcmSetting("HttpPort", property);
+        if (result && property.IsInt()) {
+            int port = property.GetSInt();
+            return port;
+        }
+        return -1;
     }
-    return -1;
-}
 
 }
